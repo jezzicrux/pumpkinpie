0a361626e0400b27b2aa2849962b3c522013f07c
Jessica Cruz <jessicamc514@gmail.com>
2017-02-07 20:18:18 -0500

    class notes day 2

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+'''There is cake in the hallway
+print ("Hello World")
+There is cake in the hallway
+print ("Goodnight Moon")
+
+There is cake in the hallway
+Hello="Hello"
+World="World"
+a=Hello
+b=World
+print (a+b)
+
+a=2
+b=3
+print (a+b)
+c="9"
+d=2
+print(float(c)+d)
+
+test1=float(input("Please input the score for Test 1. "))
+test2=int(input("Please input the score for Test 2. "))
+test3=float(input("Please input the score for Test 3. "))
+average= (test1+test2+test3)/3
+print("The Average of all 3 tests is ", average)'''
+
+print ("My name is",end="")
+print ("Bob")
+print ("My name is ",end="")
+print ("Timmy")
+print ("My name is",end=" ")
+print ("Bobby")
+print ("My name is ",end=" ")
+print ("Larry")

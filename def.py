1b1e89ebadabde46e646f4bcb765c5cf278ecd53
Jessica Cruz <jessicamc514@gmail.com>
2017-02-07 20:15:30 -0500

    class notes day 2

---
+++ def.py
@@ -0,0 +1,22 @@
+'''def blue():
+    a = float(input("What is your first value? ")) #Ask the user for the first value
+    b = float(input("What is your second value? "))
+    print('The average is: ', (a+b)/2)
+blue()
+
+a=15
+def someFunction():
+    print (a+5)
+    print (a+b)
+someFunction()
+b=float(input("Enter a number. "))'''
+
+a=int(input("Pick a number. "))
+if a >= 22:
+    print ("if")
+elif a >=7:
+    print ("elif")
+else:
+    print ("else")
+print("Hey")
+
